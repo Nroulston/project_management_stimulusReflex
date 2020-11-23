b8811e294d48aca2707a0283738da562847c7ec2
Nroulston <nicolas.roulston@gmail.com>
2020-11-23 09:17:21 -0700

    stimulus reflex

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,3 +22,5 @@ document.addEventListener('turbolinks:load', () => {
   var el = document.getElementById("tasks-list");
   var sortable = Sortable.create(el); { animation: 150 }; 
 });
+
+import "controllers"

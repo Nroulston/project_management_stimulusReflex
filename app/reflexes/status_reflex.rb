bb8baccca68daece5628dded7c8d3a4c13c7cc79
Nroulston <nicolas.roulston@gmail.com>
2020-11-24 13:14:16 -0700

    re-render only the updated item via morph

--- app/reflexes/status_reflex.rb
+++ app/reflexes/status_reflex.rb
@@ -5,6 +5,7 @@ class StatusReflex < ApplicationReflex
     def change
       task = Task.find(element.dataset[:id])
       task.update(status: element.dataset[:status])
+      morph dom_id(task), TasksController.render(TaskComponent.new(task: task))
     end
 
   # Add Reflex methods in this file.
